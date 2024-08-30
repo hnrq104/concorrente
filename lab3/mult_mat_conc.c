7d7319cf7fc7ca9f6a70c177a6953076801d18b3
henrique <henriquelc@dcc.ufrj.br>
2024-08-30 16:19:17 -0300

    estraguei o código com checagem de erros

--- lab3/mult_mat_conc.c
+++ lab3/mult_mat_conc.c
@@ -42,6 +42,10 @@ void *Multiplica_Thread_Guardando(void* v_arg){
     
     for(int j = arg->id_thread; j < c; j += arg->num_threads){
         float* coluna_B = malloc(sizeof(float) * tam);
+        if(!coluna_B){
+            fprintf(stderr,"erro: malloc continuo para coluna %d da segunda matriz\n",j);
+            exit(4);
+        }
         for(int k = 0; k < tam; k++){
             coluna_B[k] = B.m[k*c + j];
         }
@@ -84,23 +88,43 @@ void *Multiplica_Thread(void* v_arg){
 int main(int argc, char* argv[]){
     struct timeval start,finish;
     gettimeofday(&start,0);
-    //s arq1 arq2 arqsaida
+
     if(argc < 5){
         printf("Uso: %s <arq-matriz-entrada> <arq-matriz-entrada> <arq-saida> <n-threads>\n",argv[0]);
-        return 1;
+        return 5;
     }
 
     FILE* arq_mat_1 = fopen(argv[1],"rb");
+    if(!arq_mat_1){
+        fprintf(stderr,"erro: ao abrir arquivo %s\n",argv[1]);
+        return 1;
+    }
     FILE* arq_mat_2 = fopen(argv[2],"rb");
+    if(!arq_mat_1){
+        fprintf(stderr,"erro: ao abrir arquivo %s\n",argv[1]);
+        return 1;
+    }
     FILE* files[2] = {arq_mat_1,arq_mat_2};
     mat input[2];
 
     for(int i = 0; i < 2; i++){
         int l,c;
-        fread(&l,sizeof(int),1,files[i]);
-        fread(&c,sizeof(int),1,files[i]);
+        if(fread(&l,sizeof(int),1,files[i]) < 1){
+            fprintf(stderr,"erro: ao ler num de linhas da mat %d\n",i);
+            return 1;
+        }
+        if(fread(&c,sizeof(int),1,files[i]) < 1){
+            fprintf(stderr,"erro: ao ler num de colunas da mat %d\n",i);
+            return 1;    
+        }
         input[i].m = malloc(l*c*sizeof(float));        
-        fread(input[i].m,sizeof(float),(size_t) l*c,files[i]);
+        if(!input[i].m){
+            fprintf(stderr,"erro: malloc para matriz %d\n",i);
+            return 2;
+        }
+        if(fread(input[i].m,sizeof(float),(size_t) l*c,files[i]) < (size_t) l*c){
+            fprintf(stderr,"erro: ao ler valores da mat %d\n",i);
+        }
         input[i].linhas = l;
         input[i].colunas = c;
         fclose(files[i]);
@@ -108,17 +132,24 @@ int main(int argc, char* argv[]){
     
     if(input[0].colunas != input[1].linhas){
         fprintf(stderr,"erro: as matrizes [%s] [%s] sao incompativeis\n",argv[1],argv[2]);
-        return 2;
+        return 3;
     }
 
     mat saida;
     saida.linhas = input[0].linhas;
     saida.colunas = input[1].colunas;
     saida.m = malloc(sizeof(float) * saida.linhas * saida.colunas);
+    if(!saida.m){
+        printf("erro: malloc para a matriz de saida\n");
+        return 2;
+    }
 
     int num_threads = atoi(argv[4]);
     pthread_t *tids = malloc(sizeof(pthread_t) * num_threads);
-
+    if(!tids){
+        printf("erro: malloc para os tids\n");
+        return 2;
+    }
     //aqui a gente finaliza a contagem de tempo da inicialização
     gettimeofday(&finish,0);
     double elapsed_init = finish.tv_sec - start.tv_sec + (finish.tv_usec - start.tv_usec)*1e-6;
@@ -143,19 +174,33 @@ int main(int argc, char* argv[]){
     //aqui finalizamos a contagem para a multiplicacao
     gettimeofday(&finish,0);
     double elapsed_processo = finish.tv_sec - start.tv_sec + (finish.tv_usec - start.tv_usec)*1e-6;
-
-    
     gettimeofday(&start,0);
+
     FILE* arq_saida = fopen(argv[3],"wb");
-    fwrite(&saida.linhas,sizeof(int),1,arq_saida);
-    fwrite(&saida.colunas,sizeof(int),1,arq_saida);
-    fwrite(saida.m,sizeof(float), (size_t)saida.linhas*saida.colunas, arq_saida);
+    if(!arq_saida){
+        printf("erro: ao abrir o arquivo de saida %s\n",argv[3]);
+        return 1;
+    }
+    if(fwrite(&saida.linhas,sizeof(int),1,arq_saida) < 1){
+        printf("erro: ao escrever linhas da matriz de saida\n");
+        return 1;
+    }
+    if(fwrite(&saida.colunas,sizeof(int),1,arq_saida) < 1){
+        printf("erro: ao escrever colunas da matriz de saida\n");
+        return 1;
+    }
+
+    size_t saida_tam = saida.linhas*saida.colunas;
+    if(fwrite(saida.m,sizeof(float), saida_tam, arq_saida) < saida_tam){
+        printf("erro: ao escrever valores da matriz de saida\n");
+        return 1;    
+    }
     fclose(arq_saida);
 
     free(input[0].m);
     free(input[1].m);
     free(saida.m);
-
+    
     gettimeofday(&finish,0);
     double elapsed_finalizacao = finish.tv_sec - start.tv_sec + (finish.tv_usec - start.tv_usec)*1e-6;
     double elapsed_total = elapsed_init + elapsed_processo + elapsed_finalizacao;
--- lab3/multiplicar_matriz.c
+++ lab3/multiplicar_matriz.c
@@ -35,6 +35,10 @@ void Multiplica_Guardando(mat A, mat B, mat *C){
     C->colunas = c;
     for(int j = 0; j < c; j++){
         float* B_coluna = malloc(sizeof(float)*tam);
+        if(!B_coluna){
+            printf("erro: ao mallocar espaco continguo para coluna %d da segunda matriz\n",j);
+            exit(4);
+        }
         for(int k = 0; k < tam; k++){
             B_coluna[k] = B.m[k*c + j];
         }
@@ -53,21 +57,41 @@ int main(int argc, char* argv[]){
     //s arq1 arq2 arqsaida
     if(argc < 4){
         printf("Uso: %s <arq-matriz-entrada> <arq-matriz-entrada> <arq-saida>\n",argv[0]);
-        return 1;
+        return 5;
     }
 
     FILE* arq_mat_1 = fopen(argv[1],"rb");
+    if(!arq_mat_1){
+        printf("erro: ao abrir arquivo %s\n",argv[1]);
+        return 1;
+    }
     FILE* arq_mat_2 = fopen(argv[2],"rb");
+    if(!arq_mat_1){
+        printf("erro: ao abrir arquivo %s\n",argv[1]);
+        return 1;
+    }
 
     FILE* files[2] = {arq_mat_1,arq_mat_2};
     mat input[2];
 
     for(int i = 0; i < 2; i++){
         int l,c;
-        fread(&l,sizeof(int),1,files[i]);
-        fread(&c,sizeof(int),1,files[i]);
+        if(fread(&l,sizeof(int),1,files[i]) < 1){
+            printf("erro: ao ler num de linhas da mat %d\n",i);
+            return 1;
+        }
+        if(fread(&c,sizeof(int),1,files[i]) < 1){
+            printf("erro: ao ler num de colunas da mat %d\n",i);
+            return 1;    
+        }
         input[i].m = malloc(l*c*sizeof(float));        
-        fread(input[i].m,sizeof(float),(size_t) l*c,files[i]);
+        if(!input[i].m){
+            printf("erro: malloc para matriz %d\n",i);
+            return 2;
+        }
+        if(fread(input[i].m,sizeof(float),(size_t) l*c,files[i]) < (size_t) l*c){
+            printf("erro: ao ler valores da mat %d\n",i);
+        }
         input[i].linhas = l;
         input[i].colunas = c;
         fclose(files[i]);
@@ -75,13 +99,17 @@ int main(int argc, char* argv[]){
     
     if(input[0].colunas != input[1].linhas){
         printf("erro: as matrizes [%s] [%s] sao incompativeis\n",argv[1],argv[2]);
-        return 2;
+        return 3;
     }
 
     mat saida;
     saida.linhas = input[0].linhas;
     saida.colunas = input[1].colunas;
     saida.m = malloc(sizeof(float) * saida.linhas * saida.colunas);
+    if(!saida.m){
+        printf("erro: malloc para a matriz de saida\n");
+        return 2;
+    }
 
 
     // int num_threads = stoi(argv[4]);
@@ -89,10 +117,29 @@ int main(int argc, char* argv[]){
     
     Multiplica(input[0],input[1],&saida);
     FILE* arq_saida = fopen(argv[3],"wb");
-    fwrite(&saida.linhas,sizeof(int),1,arq_saida);
-    fwrite(&saida.colunas,sizeof(int),1,arq_saida);
-    fwrite(saida.m,sizeof(float), (size_t)saida.linhas*saida.colunas, arq_saida);
+    if(!arq_saida){
+        printf("erro: ao abrir o arquivo de saida %s\n",argv[3]);
+        return 1;
+    }
+    if(fwrite(&saida.linhas,sizeof(int),1,arq_saida) < 1){
+        printf("erro: ao escrever linhas da matriz de saida\n");
+        return 1;
+    }
+    if(fwrite(&saida.colunas,sizeof(int),1,arq_saida) < 1){
+        printf("erro: ao escrever colunas da matriz de saida\n");
+        return 1;
+    }
+
+    size_t saida_tam = saida.linhas*saida.colunas;
+    if(fwrite(saida.m,sizeof(float), saida_tam, arq_saida) < saida_tam){
+        printf("erro: ao escrever valores da matriz de saida\n");
+        return 1;    
+    }
     fclose(arq_saida);
+
+    free(input[0].m);
+    free(input[1].m);
+    free(saida.m);
 
     return 0;
 }   
